2bc34a0885868372688d630777191a9ceb4fdedc
swethakrishnan7399 <swetha@growth.cx>
2022-08-10 19:55:18 +0530

    updated toggle time

--- js/home.js
+++ js/home.js
@@ -7,8 +7,13 @@ async function cycle() {
     text_container.innerText = text[0];
     img_container.classList.add('hidden')
     video_container.classList.remove('hidden')
+    video_container.currentTime = 0;
+    video_container.play()
     await new Promise(r => setTimeout(r, 5400));
     text_container.innerText = text[1];
+    video_container.currentTime = 5.2;
+    video_container.pause()
+    console.log(video_container.currentTime)
     video_container.classList.add('hidden')
     img_container.classList.remove('hidden')
     await new Promise(r => setTimeout(r, 4000));
